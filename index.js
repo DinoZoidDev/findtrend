9bb1fe6187072a7d354a08436a6369e3879b42f9
dinozoiddev <realdinozoid@gmail.com>
2023-09-13 15:24:32 -0500

    trial: change image locations

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ const hamburgerMenu = document.getElementById("menu");
 hamburgerMenu.addEventListener("click", () => {
   body.dataset.mobileMenu =
     body.dataset.mobileMenu === "opened" ? "closed" : "opened";
-  document.documentElement.style.overflowY =
-    body.dataset.state === "opened" ? "hidden" : "auto";
 });
