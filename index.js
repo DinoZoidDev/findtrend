680d3d907d881332faaf04a51de47d8be937eab4
dinozoiddev <realdinozoid@gmail.com>
2023-09-19 18:20:25 -0500

    feat: fix scroll

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const body = document.body;
 const hamburgerMenu = document.getElementById("menu");
+const mobileWrapper = document.getElementById("mobile-wrapper");
+const buttons = document.getElementsByTagName("button");
 
-hamburgerMenu.addEventListener("click", () => {
+const updateDialogState = () => {
   body.dataset.mobileMenu =
     body.dataset.mobileMenu === "opened" ? "closed" : "opened";
+};
+
+hamburgerMenu.addEventListener("click", updateDialogState);
+for (let i = 0; i < buttons.length; i++) {
+  buttons.item(i).addEventListener("click", updateDialogState);
+}
+
+body.addEventListener("keydown", (e) => {
+  console.log(e.key);
+  if (e.key === "Escape" && body.dataset.mobileMenu === "opened")
+    body.dataset.mobileMenu = "closed";
 });
